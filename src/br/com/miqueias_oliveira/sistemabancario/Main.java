06e430d10b19b4bbd1bbb74e79471328b6f73b9d
maikmso <maikxsantosoli2@outlook.com>
2025-07-23 12:11:44 -0300

    Arquivos iniciais do projeto de Sistema Bancário.

---
+++ src/br/com/miqueias_oliveira/sistemabancario/Main.java
@@ -0,0 +1,8 @@
+package br.com.miqueias_oliveira.sistemabancario;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/br/com/miqueias_oliveira/sistemabancario/model/Conta.java
@@ -0,0 +1,51 @@
+package br.com.miqueias_oliveira.sistemabancario.model;
+
+public class Conta {
+
+    private String numero;
+    private String titular;
+    private double saldo;
+
+    public Conta(String numero, String titular) {
+        this.numero = numero;
+        this.titular = titular;
+        this.saldo = 0.0;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    // Deposita um valor na conta, aumentando o saldo inicial
+    public void depositar(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+        }
+    }
+
+    // Tenta sacar um valor da conta se houver saldo suficiente.
+    public boolean sacar(double valor) {
+        if (valor > 0 && saldo >= valor) {
+            saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Conta: " +
+                numero +
+                ", Titular: " +
+                titular +
+                ", Saldo: R$" + saldo;
+    }
+}
